4d220ad1bf4bc97d8557860cb750447a23e1d25c
hgxmiami <hgxmiami@aol.com>
2016-03-14 19:49:41 -0400

    update
    update

--- js/prog.js
+++ js/prog.js
@@ -3,15 +3,33 @@ $(document).ready(function(){
 });
  
 var customerAnswer = {};
+var good = 0;
+var bad = 0;
  
 $("#validate").on("click", function(e){
-    console.log(customerAnswer);
+    var counter = 0;
+    for(var x in customerAnswer){
+        $.ajax({
+        url: "http://fvi-grad.com:4004/quiz-get-answer/"+x+"",
+        success: function(resp, txt, xhr){
+             var answer = eval(resp);
+            if(customerAnswer[x] == answer)
+                good += 1;
+            else
+                bad += 1;
+               counter++;
+        }
+            
+    });
+    }
+    if(counter >= 5)
+        alert("you had " + good + " goods and " + bad + " bad");
 })
  
  
 $(".panel-body").on("click", "input", function(e){
     var id = $(this).attr("name");
-    var value = $(this).attr("value");
+    var value = $(this).val();
     if(!customerAnswer[id]){
         findAnswers(id);
     }
